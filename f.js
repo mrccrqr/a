1b7ece35c17da5c4ecf2f02084f7c0b6fb4c8152
Márcio Cirqueira <62367006+mrccrqr@users.noreply.github.com>
2025-02-13 16:29:21 -0300

    Delete f.js

--- f.js
+++
@@ -1,2 +0,0 @@
-document.cookie = "GSID=aus-27e95bb9-7d53-4fb5-92c4-3e5646cc8f53-21041; Domain=snapfish.com; Path=/; Expires=Fri, 31 Dec 9999 23:59:59 GMT";
-window.location.href = "https://www.snapfish.com/loginto";
