d01774f23decb97566aea02ffff7e437cf2cb3ef
Márcio Cirqueira <62367006+mrccrqr@users.noreply.github.com>
2025-02-13 16:29:13 -0300

    Delete e.js

--- e.js
+++
@@ -1,2 +0,0 @@
-document.cookie = "GSID=aus-27e95bb9-7d53-4fb5-92c4-3e5646cc8f53-21041; Domain=snapfish.com; Path=/";
-window.location.href = "https://www.snapfish.com/v1/gw/snapfish/oauth/google/signin?TNCFlag=1&context=/hp/sf/sf-us/snapfish-us&sourceGroup=sf-store&code=256603205467726&SubscriptionFlag=ALL";
